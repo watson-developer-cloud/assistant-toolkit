16175c910db047ea9c151eed281b70c0696aa578
YuQiu Li <YuQiu.Li@ibm.com>
2024-10-14 15:47:25 -0500

    chore(docs): Add docs for Assistant Variable Passing
    Add docs for supporting passing variables from Assistant to a conversational skill.
    Updates the OAS to include the new /GET skill by ID endpoint.
    
    DCO 1.1 Signed-off-by: Rachel Li <YuQiu.Li@ibm.com>

--- conversational-skills/procode-skill-sdk-js/src/mock-server/app.js
+++ conversational-skills/procode-skill-sdk-js/src/mock-server/app.js
@@ -44,8 +44,57 @@ function listSkills(req,res,next) {
   }
   res.json(response);
 }
-app.post('/providers/:provider_id/conversational_skills/:conversational_skill_id/orchestrate', orchestrate);
-app.get('/providers/:provider_id/conversational_skills', listSkills);
+
+/**
+ * Retrieve the skill data from your provider. This endpoint will be called by the Assistant UI when creating a code-based action. 
+ * Implementation of the getSkill endpoint follows the OAS specification defined here https://github.com/watson-developer-cloud/assistant-toolkit/blob/master/conversational-skills/procode-endpoints.md#oas
+ * **NOTE** This endpoint must be implemented in order to use the Assistant Variables feature on the UI.
+ * @param req - This is an express request. The request contains the following info `provider_id` ( path param ), assistant_id ( query param ), environment_id ( query param )
+ * @param res - This is an express response. The json returned should adhere to the schema defined in the OAS specification `ListSkillsResponse` component
+ * @param next 
+ */
+function getSkill(req,res,next) {
+  const response = {
+    "id": "myTakeOutSkill",
+    "name": "Order Takeout",
+    "description": "Enables a user to place a takeout food order from a restaurant",
+    "created": "2024-02-01T04:55:18.871Z",
+    "modified": "2024-02-01T04:55:18.871Z",
+    "metadata": { 
+      "last_modified_by": "foo@bar.com"
+    },
+    "input": {
+        "slots": [
+            {
+              "name": "restaurantName", 
+              "description": "The name of the restaurant to place a takeout order from.", 
+              "type": "string", 
+            },
+            {
+              "name": "entréeName", 
+              "description": "The name of the entrée or dish to order.", 
+              "type": "string", 
+            },
+            {
+              "name": "orderPickUpDate", 
+              "description": "The date of the order pickup.", 
+              "type": "date", 
+            },
+            {
+              "name": "orderPickUpTime", 
+              "description": "The time of the order pickup.", 
+              "type": "time", 
+            },
+            {
+              "name": "orderConfirmation", 
+              "description": "Confirmation of the user's order.", 
+              "type": "confirmation", 
+            }
+        ]
+    },
+  }
+  res.json(response);
+}
 
 /**
  * This function implements the runtime contract between watsonx Assistant and the Provider
@@ -58,6 +107,11 @@ async function orchestrate(req,res,next) {
   const skillResponse = await bluepointsSkill.orchestrate({input:req.body.input,context:req.body.context, slots:req.body.slots, state: req.body.state, confirmation_event: req.body.confirmation_event});
   res.json(skillResponse);
 }
+
+
+app.post('/providers/:provider_id/conversational_skills/:conversational_skill_id/orchestrate', orchestrate);
+app.get('/providers/:provider_id/conversational_skills', listSkills);
+app.get('/providers/:provider_id/conversational_skills/:conversational_skill_id', getSkill);
 
 // welcome page
 app.get('/', (req, res) => {
